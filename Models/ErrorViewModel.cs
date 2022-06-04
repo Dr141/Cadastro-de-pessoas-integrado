5bf0aa35311fbc7ffe5d0d6e9051ef768bef072d
Rhemerson <lira_Rhemerson@hotmail.com>
2022-06-04 13:08:10 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cadastro_de_pessoas_integrado.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
